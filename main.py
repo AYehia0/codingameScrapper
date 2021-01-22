98852c4269923993f89e9dd4ccefdbd7b458a860
AYehia0 <ahmedyehia611@gmail.com>
2021-01-22 22:19:06 +0200

    ignored data related to the login credentials

---
+++ main.py
@@ -0,0 +1,22 @@
+# LOL website is down
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.chrome.options import Options
+
+# For testing
+import requests
+
+
+link = "https://www.codingame.com/ide/354506029a26c09f843bad81165aa0c423daa338"
+
+#init the webdriver
+driver = webdriver.Chrome()
+
+#login is needed only the first time
+options = webdriver.ChromeOptions()
+options.add_argument('--user-data-dir=./User_Data')
+driver = webdriver.Chrome(executable_path='chromedriver', options=options)
+wait = WebDriverWait(driver, 5)
+
+driver.get(link)
