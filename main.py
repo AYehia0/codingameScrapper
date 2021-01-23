ff2b06a9416505ed60afcba8b7e773672cef0715
AYehia0 <ahmedyehia611@gmail.com>
2021-01-23 23:39:43 +0200

    This is how to save login and use it [REF]

--- main.py
+++ main.py
@@ -11,39 +11,45 @@ import requests
 
 
 #link = "https://www.codingame.com/clashofcode/clash/1558452d871d4d157e24ee47c16a7492db3258b"
-link = "https://www.codingame.com/ide/35478861c775e56b0e180f7aea83f25c2cc25a4b"
+
+some_other_link = "https://www.google.com"
+link = "https://www.codingame.com/"
 
 
 #login is needed only the first time
-# options = webdriver.ChromeOptions()
+options = webdriver.ChromeOptions()
 
 # # Here i am using brave's login credentials so that i don't have to login 
-# options.add_argument('--user-data-dir=/home/none/.config/BraveSoftware/Brave-Browser/')
-driver = webdriver.Chrome(executable_path='chromedriver')
+options.add_argument('--user-data-dir=./User_Data')
+driver = webdriver.Chrome(executable_path='chromedriver', options=options)
 
+
+#driver.get(some_other_link)
+time.sleep(3)
 driver.get(link)
+
 
 # List of test cases in case of reverse mode:  cg-ide-testcases-details-reverse
 # Case open : testcase open
 # test Case content : testcase-content
 
-time.sleep(10)
 
 
-#closing the welcome window
-driver.find_element_by_class_name("got-it-button").click()
+
+# #closing the welcome window
+# driver.find_element_by_class_name("got-it-button").click()
 
 
-#title xpath
-# 
+# #title xpath
+# # 
 
-test_cases = driver.find_elements_by_class_name("cg-ide-testcases-details-reverse")
+# test_cases = driver.find_elements_by_class_name("cg-ide-testcases-details-reverse")
 
-print(len(test_cases))
-for case in test_cases:
-    co = case.find_element_by_class_name("testcase open")
-    for content in co:
-        print(content.find_element_by_class_name("testcase-content"))
+# print(len(test_cases))
+# for case in test_cases:
+#     co = case.find_element_by_class_name("testcase open")
+#     for content in co:
+#         print(content.find_element_by_class_name("testcase-content"))
 
 # /html/body/div[8]/div[2]/div[1]/div/div/ide-page/div/div[2]/div[4]/div[4]/div[2]/div/div[2]/div[2]/div[2]/div[1]/div[2]/div[2]
 # /html/body/div[8]/div[2]/div[1]/div/div/ide-page/div/div[2]/div[4]/div[4]/div[2]/div/div[2]/div[2]/div[2]/div[1]/div[2]/div[1]
